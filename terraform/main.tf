d4257307408eee96b998d424b1bad95b276f8ed2
Aaron Watson <aaron@thedc.space>
2026-09-22 13:32:10 +0100

    FIrst commit

---
+++ terraform/main.tf
@@ -0,0 +1,46 @@
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "public_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "allowed_ssh_ips" {
+  type = list(string)
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = "my-project-rg"
+  location = "uksouth"
+}
+
+module "vm" {
+  source = "git::https://github.com/DogsbodyOps/tf-templates.git//azure/modules/simple-vm?ref=main"
+
+  name                = "kiddie-pbx"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  admin_username      = var.admin_username
+  public_key          = var.public_key
+  allowed_ssh_ips     = var.allowed_ssh_ips
+}
+
+output "ssh" {
+  value = module.vm.ssh_command
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,5 @@
+subscription_id = "00000000-0000-0000-0000-000000000000"
+tenant_id       = "00000000-0000-0000-0000-000000000000"
+admin_username  = "azureuser"
+public_key      = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC... you@host"
+allowed_ssh_ips = ["217.155.49.58/32","2a02:8010:6832:1::/64"]
